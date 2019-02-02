05e6163048865c77e3bfbf5aa68e7785173a4bef
elliottxharris <exharris1@shockers.wichita.edu>
2019-02-02 09:44:34 -0600

    Initial Commit

---
+++ Insertion Sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Insertion Sort
+//
+//  Created by Elliott Harris on 2/2/19.
+//  Copyright © 2019 Elliott. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
